b3ba5d5ab4dcd1de53cc989208994c0526abbd24
Clueless365 <mgudel03@fesb.hr>
2021-12-15 13:02:27 +0100

    Dodan 7. i 8. bez lvlOrdera

---
+++ zdtk 7/zdtk 7/Source.c
@@ -0,0 +1,302 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#define MAX 128
+#define MAX_LINE 1024
+
+struct MyStruct;
+typedef struct MyStruct* pos;
+typedef struct MyStruct
+{
+	char name[MAX];
+	pos parent;
+	pos sibling;
+	pos child;
+}tree;
+//tree predstavlja direktorije, root je C: direktorij,
+//svaki direktorij ima jednog childa, osim ako je na dnu stabla onda je NULL,
+//te svaki direktorij ima parenta osim root-a, a sibling funkcioniraju kao stack
+//prvi napravljeni direktorij nema siblinga,  a sljedeci ce pokazivat na prvog 
+//treci na drugog i tako dalje dok se ne napravi novi level i onda ispocetka
+struct MyStruct2;
+typedef struct MyStruct2* Spos;
+typedef struct MyStruct2
+{
+	char Sname[MAX_LINE];
+	Spos next;
+	
+}stack;
+/*Ova struktura predstavlja trnutunu lokaciju, svaki put kad se pozove cd,
+ako je uspjesan, na kraj liste se stavlja ime direktorija, ako se pozove cdBack
+kraj se brise*/
+int displayLocation(Spos head);
+pos createDir(pos NewDir, char* name);
+pos makeRootDir();
+Spos createStackEl(char* name);
+Spos findLastStackEl(Spos head);
+int md(char* name, pos current);
+pos cd(char* name, pos current, Spos head);
+pos cdBack(pos current, Spos head);
+int dir(pos current);
+Spos createHeadStackEl();
+int deleteLastStackEl(Spos head);
+int menu(pos current, Spos head);
+
+int main()
+{
+	pos root = NULL;
+	Spos head = NULL;
+	root = makeRootDir();
+	head = createHeadStackEl();
+
+	menu(root, head);
+
+	return 0;
+}
+pos createDir(pos NewDir, char* name)
+{
+	NewDir = (pos)malloc(sizeof(tree));
+	strcpy(NewDir->name, name);
+	NewDir->parent = NULL;
+	NewDir->sibling = NULL;
+	NewDir->child = NULL;
+
+	return NewDir;
+}
+
+
+pos makeRootDir()
+{
+	pos NewDir = NULL;
+	NewDir = (pos)malloc(sizeof(tree));
+
+	strcpy(NewDir->name, "C:/");
+	NewDir->parent = NULL;
+	NewDir->sibling = NULL;
+	NewDir->child = NULL;
+
+	return NewDir;
+}
+Spos createStackEl(char* name)
+{
+	Spos NewEl = NULL;
+	NewEl = (Spos)malloc(sizeof(stack));
+
+	strcpy(NewEl->Sname, "/");
+	strcat(NewEl->Sname, name);
+	//strcat(NewEl->Sname, " ");
+
+	NewEl->next = NULL;
+
+	return NewEl;
+}
+Spos findLastStackEl(Spos head)
+{
+	Spos temp = head;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	return temp;
+}
+
+int md(char* name, pos current)
+{
+	pos temp = current;
+	pos NewDir = NULL;
+
+	if (temp->child == NULL) {
+		NewDir = createDir(NewDir, name);
+		temp->child = NewDir;
+		NewDir->parent = temp;
+
+		return EXIT_SUCCESS;
+	}
+	if (temp->child != NULL) {
+		NewDir = createDir(NewDir, name);
+		NewDir->sibling = temp->child;
+		//temp->child->parent = NULL;
+		temp->child = NewDir;
+		NewDir->parent = temp;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+pos cd(char* name, pos current, Spos head)
+{
+	pos temp = NULL;
+	Spos last = NULL;
+	Spos newLast = NULL;
+
+	if (current->child == NULL) {
+		printf("Prazan direktorij!\n");
+		return current;
+	}
+
+	if (strcmp(current->child->name, name) == 0) {
+		current = current->child;
+
+		last = findLastStackEl(head);
+		newLast = createStackEl(name);
+		newLast->next = last->next;
+		last->next = newLast;
+
+		return current;
+	}
+
+	temp = current->child;
+	temp = temp->sibling;
+
+	while (temp)
+	{
+		if (strcmp(temp->name, name) == 0)
+		{
+			current = temp;
+
+			last = findLastStackEl(head);
+			newLast = createStackEl(name);
+			newLast->next = last->next;
+			last->next = newLast;
+
+			return current;
+		}
+		else
+			temp = temp->sibling;
+	}
+
+	printf("Nema tog dir-a\n");
+	return current;
+
+}
+
+pos cdBack(pos current, Spos head)
+{
+	Spos temp = head;
+	
+	if (current->parent == NULL)
+	{
+		printf("Dosli ste do root dir-a\n");
+		return current;
+	}
+
+	current = current->parent;
+
+	deleteLastStackEl(temp);
+
+	return current;
+}
+
+int dir(pos current)
+{
+	printf("\==============================\n");
+	pos temp = NULL;
+	if (current->child == NULL) {
+		printf("==============================\n");
+		return EXIT_SUCCESS;
+	}
+
+	temp = current->child;
+
+	while (temp != NULL)
+	{
+		printf("%s\n", temp->name);
+
+		temp = temp->sibling;
+	}
+	printf("==============================\n");
+	return EXIT_SUCCESS;
+}
+
+Spos createHeadStackEl()
+{
+	Spos NewEl = NULL;
+	NewEl = (Spos)malloc(sizeof(stack));
+
+	strcpy(NewEl->Sname, ">C:/");
+	NewEl->next = NULL;
+
+	return NewEl;
+}
+
+
+int deleteLastStackEl(Spos head)
+{
+	Spos temp = head;
+	Spos last = NULL;
+	while (temp->next->next != NULL)
+		temp = temp->next;
+
+	last = temp->next;
+	temp->next = NULL;
+	free(last);
+
+	return EXIT_SUCCESS;
+}
+
+int menu(pos current, Spos head)
+{
+	Spos temp = head;
+	char name[MAX_LINE] = { 0 };
+	char command[MAX_LINE] = { 0 };
+	char line[MAX_LINE] = { 0 };
+	printf(">C:/ ");
+
+
+	while (1)
+	{
+		fgets(line, MAX_LINE, stdin);
+		sscanf(line, "%s %s", command, name);
+
+		if (!strcmp(command, "md"))
+		{
+			md(name, current);
+			displayLocation(temp);
+			printf(" ");
+			
+			//printf("Current= %s\nCurrent->child=%s\n",current->name,current->child->name);
+		}
+		else if (!strcmp(command, "cd"))
+		{
+			current = cd(name, current, temp);
+			displayLocation(temp);
+			printf(" ");
+		}
+		else if (!strcmp(command, "cd.."))
+		{
+			//ne smi izac iz C:
+			current = cdBack(current, temp);
+			displayLocation(temp);
+			printf(" ");
+
+		}
+		else if (!strcmp(command, "dir"))
+		{
+			dir(current);
+			displayLocation(temp);
+			printf(" ");
+		}
+		else if (!strcmp(command, "exit"))
+		{		
+			break;
+		}
+		else
+		{
+			printf("'%s' nije prepoznano kao naredba!\n", command);
+		}
+	}
+	return EXIT_SUCCESS;
+}
+int displayLocation(Spos head)
+{
+	Spos temp=head;
+
+	while (temp)
+	{
+		printf("%s", temp->Sname);
+		temp = temp->next;
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ zdtk 8/zdtk 8/Source.c
@@ -0,0 +1,244 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+struct MyStruct;
+typedef struct MyStruct* pos;
+typedef struct MyStruct
+{
+	int num;
+	pos right;
+	pos left;
+} tree;
+
+/*Koristen kod kao reference pa nije 100% original*/
+
+pos createNew(int num);
+pos insertEl(int num, pos root);
+int printInOrder(pos root);
+int printPreOrder(pos root);
+int printPostOrder(pos root);
+pos findMin(pos root);
+pos findTree(pos root, int num);
+pos deleteTree(pos root, int num);
+int menu(pos root);
+
+int main()
+{
+	pos root = NULL;
+	menu(root);
+
+	return EXIT_SUCCESS;
+}
+
+pos createNew(int num)
+{
+	pos newEl = (pos)malloc(sizeof(tree));
+	if (newEl == NULL)
+	{
+		printf("Malloc greska\n");
+		return NULL;
+	}
+
+	newEl->num = num;
+	newEl->right = NULL;
+	newEl->left = NULL;
+
+	return newEl;
+}
+
+pos insertEl(int num, pos root)
+{
+	if (!root)
+		return createNew(num);
+
+	else if (num < root->num)
+		root->left = insertEl(num, root->left);
+
+	else if (num > root->num)
+		root->right = insertEl(num, root->right);
+
+	return root;
+}
+
+int printInOrder(pos root)
+{
+	if (root != NULL)
+	{
+		printInOrder(root->left);
+		printf("%d\n", root->num);
+		printInOrder(root->right);
+	}
+	return EXIT_SUCCESS;
+}
+
+int printPreOrder(pos root)
+{
+	if (root != NULL)
+	{
+		printf("%d\n", root->num);
+		printPreOrder(root->left);
+		printPreOrder(root->right);
+	}
+	return EXIT_SUCCESS;
+}
+
+int printPostOrder(pos root)
+{
+	if (root != NULL)
+	{
+		printPostOrder(root->left);
+		printPostOrder(root->right);
+		printf("%d\n", root->num);
+	}
+	return EXIT_SUCCESS;
+}
+
+pos deleteTree(pos root, int num)
+{
+	pos temp;
+
+	if (root == NULL)
+		return NULL;
+
+	else if (num < root->num)
+		root->left = deleteTree(root->left, num);
+
+	else if (num > root->num)
+		root->right = deleteTree(root->right, num);
+
+	else
+	{
+		if (root->left && root->right)
+		{
+			temp = findMin(root->right);
+			root->num = temp->num;
+			root->right = deleteTree(root->right, temp->num);
+		}
+		else
+		{
+			temp = root;
+			if (root->left == NULL )
+				root = root->right;
+
+			else if (root->right == NULL )
+				root = root->left;
+
+			free(temp);
+		}
+	}
+
+	return root;
+}
+
+pos findMin(pos root)
+{
+	while (root->left)
+		root = root->left;
+
+	return root;
+}
+
+pos findTree(pos root, int num)
+{
+	if (root == NULL)
+		return NULL;
+
+	else if (num < root->num)
+		return findTree(root->left, num);
+
+	else if (num > root->num)
+		return findTree(root->right, num);
+
+	else
+		return root;
+}
+int menu(pos root)
+{
+	int choice = 0;
+	int num = 0;
+	int check = 0;
+	pos temp = NULL;
+
+	while (1)
+	{
+		printf(">IZBOR:\n"
+			"1 - Dodaj novi element\n"
+			"2 - Inorder print\n"
+			"3 - Preorder print\n"
+			"4 - postorder print\n"
+			"5 - Pronadi element\n"
+			"6 - Izbrisi element\n"
+			"0 - exit\n");
+		printf("> ");
+		scanf(" %d", &choice);
+
+		switch (choice)
+		{
+		case 1:
+			printf("Unesi br: ");
+			check = scanf(" %d", &num);
+			if (check != 1)
+			{
+				printf("Greska \nexiting...");
+				return EXIT_FAILURE;
+			}
+
+			root = insertEl(num, root);
+			break;
+
+		case 2:
+			printInOrder(root);
+			break;
+
+		case 3:
+			printPreOrder(root);
+			break;
+
+		case 4:
+			printPostOrder(root);
+			break;
+
+		case 5:
+			printf("Pronadi br: ");
+			check = scanf(" %d", &num);
+			if (check != 1)
+			{
+				printf("Greska \nexiting...");
+				return EXIT_FAILURE;
+			}
+
+
+			temp = findTree(root, num);
+
+			if (temp != NULL)
+				printf("Naden br %d na mjestu %p\n", temp->num, temp);
+			else
+				printf("Nije pronaden\n");
+
+			break;
+
+		case 6:
+			printf("Izbrisi br: ");
+			check = scanf(" %d", &num);
+			if (check != 1)
+			{
+				printf("Greska \nexiting...");
+				return EXIT_FAILURE;
+			}
+
+
+			root = deleteTree(root, num);
+			break;
+
+		case 0:
+			printf("exiting...\n\n");
+			return EXIT_SUCCESS;
+
+		default:
+			menu(root);
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
